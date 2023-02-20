32affcbdb30138bb190c35e51e928a592e716a78
Nickita <118798761+NickitaSh@users.noreply.github.com>
2023-02-20 12:42:52 +0500

    Add solution for Hard_Task2, Hard_Task4, Hard_Task6, Hard_Task8

---
+++ Sem1_Hard_Task2.py
@@ -0,0 +1,6 @@
+number = list(input('Введите трехзначное число:\n'))
+sum = 0
+
+for i in number:
+    sum += int(i)
+print(sum)
---
+++ Sem1_Hard_Task4.py
@@ -0,0 +1,6 @@
+total_count = int(input('Введите общее количество журавликов: \n'))
+x = total_count / 6
+Petya_count, Katya_count, Sergey_count = x, 4*x, x
+print(f'Петя сделал: {Petya_count}')
+print(f'Катя сделала: {Katya_count}')
+print(f'Сергей сделал: {Sergey_count}')
